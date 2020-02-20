e899248c355b0632450477812b495f10ac241aab
user <janithawijesinghe1992@gmail.com>
2020-02-20 09:58:58 +0200

    202002020_09-50

---
+++ diary_20200220.py
@@ -0,0 +1,235 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5==5
+True
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+Traceback (most recent call last):
+  File "/home/user/RTR108/test1.py", line 4, in <module>
+    if x%2 == 0:
+NameError: name 'x' is not defined
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+Traceback (most recent call last):
+  File "/home/user/RTR108/test1.py", line 4, in <module>
+    if x%2 == 0:
+NameError: name 'x' is not defined
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+3
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is even
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+small
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+small
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+lager
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x is less than y
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x is less than y
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x is less than y
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x and y are equal
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x and y are equal
+Traceback (most recent call last):
+  File "/home/user/RTR108/test1.py", line 19, in <module>
+    if choice == 'a':
+NameError: name 'choice' is not defined
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+Traceback (most recent call last):
+  File "/home/user/RTR108/test1.py", line 5, in <module>
+    choice = a
+NameError: name 'a' is not defined
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x and y are equal
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x and y are equal
+x and y are equal
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x and y are equal
+x and y are equal
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x is grater than y
+x is grater than y
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is odd
+x is lager
+x is grater than y
+x is grater than y
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is even
+x is small
+x is less than y
+x is less than y
+x is a possitve single digit number
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is even
+x is small
+x is less than y
+x is less than y
+x is a possitve single digit number
+x is a possitve single digit number
+>>> 
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is even
+x is small
+x is less than y
+x is less than y
+x is a possitve single digit number
+x is a possitve single digit number
+What is the air velocity of an unladen swallow?
+
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is even
+x is small
+x is less than y
+x is less than y
+x is a possitve single digit number
+x is a possitve single digit number
+What is the air velocity of an unladen swallow?
+
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is even
+x is small
+x is less than y
+x is less than y
+x is a possitve single digit number
+x is a possitve single digit number
+What is the air velocity of an unladen swallow?
+
+==================== RESTART: /home/user/RTR108/test1.py ====================
+True
+False
+x is even
+x is small
+x is less than y
+x is less than y
+x is a possitve single digit number
+x is a possitve single digit number
+What is the air velocity of an unladen swallow?
--- test1.py
+++ test1.py
@@ -1,161 +1,39 @@
-Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
-[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 5 ==5
-True
->>> 5 ==6
-False
->>> type(true)
-Traceback (most recent call last):
-  File "<pyshell#2>", line 1, in <module>
-    type(true)
-NameError: name 'true' is not defined
->>> 
->>> x = 5
-if x < 10:
-    print('Smaller')
-if x > 20:
-    print('Bigger')
+print(5==5)
+print(5==6)
+x = 2
+y = 21
+if x%2 == 0:
+    print('x is even')
+else:
+    print('x is odd')
+if x < 20:
+    print('x is small')
+else:
+    print('x is lager')
+if x < y:
+    print('x is less than y')
+elif x > y:
+    print('x is grater than y')
+else:
+    print ('x and y are equal')
+if x == y:
+    print('x and y are equal')
+else:
+    if x < y:
+        print('x is less than y')
+    else:
+        print('x is grater than y')
+if 0 < x:
+    if x < 10:
+        print('x is a possitve single digit number')
+if 0 < x and x < 10:
+     print('x is a possitve single digit number')
+     
+prompt = "What is the air velocity of an unladen swallow?\n"
+speed = input(prompt)
+int (speed)
 
-print('Finis')
-SyntaxError: multiple statements found while compiling a single statement
->>> type (true)
-Traceback (most recent call last):
-  File "<pyshell#5>", line 1, in <module>
-    type (true)
-NameError: name 'true' is not defined
->>> type(5==5)
-<class 'bool'>
->>> type(5==6)
-<class 'bool'>
->>> x ! = y
-SyntaxError: invalid syntax
->>> 17 and tru
-Traceback (most recent call last):
-  File "<pyshell#9>", line 1, in <module>
-    17 and tru
-NameError: name 'tru' is not defined
->>> x > 0 and x < 10
-Traceback (most recent call last):
-  File "<pyshell#10>", line 1, in <module>
-    x > 0 and x < 10
-NameError: name 'x' is not defined
->>> if x > 0:
-	print(5)
-
-	
-Traceback (most recent call last):
-  File "<pyshell#13>", line 1, in <module>
-    if x > 0:
-NameError: name 'x' is not defined
->>> x = 5
->>> x
-5
->>> print(5)
-5
->>> if x < 0
-SyntaxError: invalid syntax
->>> if x < 0:
-	pass
-
->>> x = 3
->>> if x < 10
-SyntaxError: invalid syntax
->>> if x < 10:
-	print(8)
-
-	
-8
->>> x = 3
->>> if x < 10:
-	print(6)
-	print(9)
-  File "<stdin>",line 3
-  
-SyntaxError: unindent does not match any outer indentation level
->>> print(9)
-9
->>> print(11)
-11
->>> x = 5
-if x == 5 : 
-    print('Equals 5')
-if x > 4 : 
-   print('Greater than 4')
-if  x >= 5 :
-    print('Greater than or Equals 5')
-if x < 6 : print('Less than 6') 
-if x <= 5 :
-    print('Less than or Equals 5')
-if x != 6 :
-    print('Not equal 6')
-    
-SyntaxError: multiple statements found while compiling a single statement
->>> x = 5
-if x == 5 : 
-    print(5)
-
-    
-if x > 4 : 
-   print(6)
-if  x >= 5 :
-    print(5)
-if x < 6 : print('Less than 6') 
-if x <= 5 :
-    print(4)
-if x != 6 :
-    print('8)
-	  
-SyntaxError: multiple statements found while compiling a single statement
->>> x = 5
-	  
->>> if x == 5:
-	  print (5)
-
-	  
-5
->>> if x > 4:
-	  print(7)
-
-	  
-7
->>> if x > = 5:
-	  
-SyntaxError: invalid syntax
->>> print(7)
-	  
-7
->>> 
-	  
->>> if x <= 6
-	  
-SyntaxError: invalid syntax
->>> if x <= 6:
-	  print(9)
-
-	  
-9
->>>  x = 5
- if x > 2 :
-     print(5)
-     print(3)
- print(2)
-	  
-SyntaxError: unexpected indent
->>> if x%2 == 0:
-	  print(4)
-
-	  
->>> if x%2 == 0 :
-	  print(4)
-
-	  
->>> else:
-	  
-SyntaxError: invalid syntax
->>> if x%2 == 0:
-	  print(4)
-	  else :
-	  
-SyntaxError: invalid syntax
->>> 
+inp = input('Enter Fahrenheit Temperature: ')
+fahr = float(inp)
+cel = (fahr - 32.0) * 5.0 / 9.0
+print(cel)
